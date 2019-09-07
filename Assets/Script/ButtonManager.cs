6cc901a085eee6d33c1fe314562f28e92bc7f93f
Gcol <colletto.greogry@gmail.com>
2019-09-07 14:44:37 +0200

    Ajout de la base

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+          SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    public void Quit () {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit ();
+    #endif
+    }
+}
+
+RE dras my life 
